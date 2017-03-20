8d47fb0b9c3b0a28d8c428864969aff60323c5d1
Caider08 <caidbuch@ksu.edu>
2017-03-19 22:25:06 -0500

    Handled Blank Search + 2 Bonus Missions

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -6,32 +6,45 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        //[HttpGet]
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+            ViewBag.selection = "location";
             return View();
         }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        //[HttpPost]
         public IActionResult Results(string searchType, string searchTerm)
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.tilte = "Search";
-            
-            if (searchType == "all")
+            ViewBag.selection = searchType;
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
             {
-                List<Dictionary<string, string>> jobz = JobData.FindByValue(searchTerm);
-                ViewBag.jobs = jobz;
-                
+                return View("Views/Search/Index.cshtml");
             }
             else
             {
-                List<Dictionary<string, string>> jobz = JobData.FindByColumnAndValue(searchType, searchTerm);
-                ViewBag.jobs = jobz;
+                if (searchType == "all")
+                {
+                    List<Dictionary<string, string>> jobz = JobData.FindByValue(searchTerm);
+                    ViewBag.jobs = jobz;
+
+                }
+                else
+                {
+                    List<Dictionary<string, string>> jobz = JobData.FindByColumnAndValue(searchType, searchTerm);
+                    ViewBag.jobs = jobz;
+                }
+                return View("Views/Search/Index.cshtml");
             }
-            return View("Views/Search/Index.cshtml");
-        }
+
+
+            }
     }
 }   
